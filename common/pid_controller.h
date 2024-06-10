1df8d082f8b95cc1ece31122e72df4df1e0c2616
Sidney Lin <sidney.hrlin.@gmail.com>
2024-06-11 00:01:52 +0800

    pref(control): add lpf for differential term

--- common/pid_controller.h
+++ common/pid_controller.h
@@ -21,6 +21,7 @@ struct PIDControllerConf {
 
     std::optional<std::pair<double, double>> cmd_bound;
     std::optional<double> kb;
+    std::optional<double> diff_lpf_cutoff_freq;
 };
 
 class PIDController {
@@ -31,29 +32,35 @@ class PIDController {
             auto [lb, ub] = conf_.cmd_bound.value();
             assert(lb < ub);
         }
+        if (conf_.diff_lpf_cutoff_freq.has_value()) {
+            const double wc = 2.0 * M_PI * conf_.diff_lpf_cutoff_freq.value();
+            alpha_ = 1.0 / (1.0 + wc * conf_.dt);
+        }
     }
 
     double Control(double error) {
+        double diff = 0.0;
         if (first_hit_) {
-            prev_error_ = error;
             first_hit_ = false;
+        } else {
+            diff = (1.0 - alpha_) * (error - prev_error_) / conf_.dt +
+                   alpha_ * prev_diff_;
         }
 
         // calculate integral term with anti-windup algorithms
         switch (conf_.anti_windup_method) {
             case AntiWindupMethod::CLAMPING:
-                CalIntegralWithClamping(error);
+                CalIntegralWithClamping(error, diff);
                 break;
             case AntiWindupMethod::BACK_CALCULATION:
-                CalIntegralWithBackCalculation(error);
+                CalIntegralWithBackCalculation(error, diff);
                 break;
             default:
                 CalIntegral(error);
         }
 
         // calculate command
-        double cmd = conf_.kp * error + integral_ +
-                     conf_.kd * (error - prev_error_) / conf_.dt;
+        double cmd = conf_.kp * error + integral_ + conf_.kd * diff;
 
         // applu command saturation
         if (conf_.cmd_bound.has_value()) {
@@ -68,12 +75,14 @@ class PIDController {
 
         // update internal states
         prev_error_ = error;
+        prev_diff_ = diff;
 
         return cmd;
     }
 
     void Reset() {
         prev_error_ = 0.0;
+        prev_diff_ = 0.0;
         integral_ = 0.0;
         first_hit_ = true;
     }
@@ -83,11 +92,10 @@ class PIDController {
         integral_ += (conf_.ki * error * conf_.dt);
     }
 
-    void CalIntegralWithClamping(const double error) {
+    void CalIntegralWithClamping(const double error, const double diff) {
         assert(conf_.cmd_bound.has_value());
         const double cmd = conf_.kp * error + integral_ +
-                           conf_.ki * error * conf_.dt +
-                           conf_.kd * (error - prev_error_) / conf_.dt;
+                           conf_.ki * error * conf_.dt + conf_.kd * diff;
         auto [cmd_lb, cmd_ub] = conf_.cmd_bound.value();
         if (const bool is_cmd_saturated = (cmd < cmd_lb) || (cmd > cmd_ub);
             is_cmd_saturated && (error * cmd) > 0.0) {
@@ -98,12 +106,11 @@ class PIDController {
         }
     }
 
-    void CalIntegralWithBackCalculation(const double error) {
+    void CalIntegralWithBackCalculation(const double error, const double diff) {
         assert(conf_.kb.has_value());
         assert(conf_.cmd_bound.has_value());
         const double cmd = conf_.kp * error + integral_ +
-                           conf_.ki * error * conf_.dt +
-                           conf_.kd * (error - prev_error_) / conf_.dt;
+                           conf_.ki * error * conf_.dt + conf_.kd * diff;
         auto [cmd_lb, cmd_ub] = conf_.cmd_bound.value();
         const double offset = (cmd > cmd_ub)   ? (cmd_ub - cmd)
                               : (cmd < cmd_lb) ? (cmd_lb - cmd)
@@ -116,7 +123,11 @@ class PIDController {
 
     double prev_error_{0.0};
 
+    double prev_diff_{0.0};
+
     double integral_{0.0};
+
+    double alpha_{0.0};
 
     bool first_hit_{true};
 
